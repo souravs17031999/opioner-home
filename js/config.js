2cf5a2de53d2ab1f3e16b73bc4e39e4f7aa04d1a
souravs17031999 <souravs_1999@rediffmail.com>
2021-08-16 15:06:02 +0530

    corrected prod links for backend api

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var configTestEnv = {
-    "authServiceHost" : "https://auth-service-taskly-staged.herokuapp.com/", 
-    "userServiceHost" : "https://user-service-taskly-staged.herokuapp.com/",
-    "productServiceHost": "https://product-service-taskly-staged.herokuapp.com/",
-    "notificationServiceHost": "https://notification-taskly-staged.herokuapp.com/"
+    "authServiceHost" : "https://auth-service-taskly-staged.herokuapp.com", 
+    "userServiceHost" : "https://user-service-taskly-staged.herokuapp.com",
+    "productServiceHost": "https://product-service-taskly-staged.herokuapp.com",
+    "notificationServiceHost": "https://notification-taskly-staged.herokuapp.com"
 }
