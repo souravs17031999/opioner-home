4834345262cb11bb2287d3483b17444834dd5160
Sourav Kumar <souravs_1999@rediffmail.com>
2022-01-15 21:16:58 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var configTestEnv = {
-    "authServiceHost" : "http://127.0.0.1:8081", 
-    "userServiceHost" : "http://127.0.0.1:8082",
-    "productServiceHost": "http://127.0.0.1:8083",
-    "notificationServiceHost": "http://127.0.0.1:8084"
+    "authServiceHost" : "https://auth-service-prd.herokuapp.com", 
+    "userServiceHost" : "https://user-service-prd01.herokuapp.com",
+    "productServiceHost": "https://product-service-prd.herokuapp.com",
+    "notificationServiceHost": "https://notification-service-prd.herokuapp.com"
 }
