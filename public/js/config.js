b77be9513b8f6e9afba4b061636f3e6f193b8a63
souravs17031999 <souravs_1999@rediffmail.com>
2022-04-03 16:05:32 +0530

    Bugfix/Add config.js for git to track

---
+++ public/js/config.js
@@ -0,0 +1,6 @@
+var configTestEnv = {
+    "authServiceHost" : "http://localhost:8081", 
+    "userServiceHost" : "http://localhost:8082",
+    "productServiceHost": "http://localhost:8083",
+    "notificationServiceHost": "http://localhost:8084"
+}
