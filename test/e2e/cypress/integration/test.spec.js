50d09fb328f059e6786026c6f370207abe1ddc19
souravs17031999 <souravs_1999@rediffmail.com>
2022-04-06 23:21:58 +0530

    Feature/Add Cypress GUI tests

--- test/e2e/cypress/integration/test.spec.js
+++ test/e2e/cypress/integration/test.spec.js
@@ -1,5 +1,5 @@
 describe('Opioner GUI app', () => {
-    context('Basic test', () => {
+    context('Basic Home page test', () => {
 
         before(() => {
 
@@ -16,12 +16,23 @@ describe('Opioner GUI app', () => {
             cy.wait('@GStatus')
         })
           
-        it('test #1', () => {
-            cy.contains('Login')
-        })
-        
-        it('test #2', () => {
-            cy.contains('Login')
+        it('test #1: Basic components test', () => {
+            cy.get('custom-single-header').should('have.length', 1);
+            cy.get('custom-landing-intro-section').should('have.length', 5);
+            cy.get('login-container').should('have.length', 1);
+            cy.get('custom-footer').should('have.length', 1);
+            cy.get('a').contains('Login');
+            cy.get('a').contains('Signup');
+            cy.get('a').contains('Sign up!');
+            cy.get('a').contains('Check Status ?');
+            cy.get('a').contains('Contact Us');
+            cy.get('form');
+            cy.get('h1').contains('Sign in');
+            cy.get('.username-container').find('label').contains('Username');
+            cy.get('.password-outer-container').find('label').contains('Password');
+            cy.get('button').contains('Login');
+            cy.get('a').contains('Forgotten password?');
+            cy.get('button').contains('Create New Account');
         })
 
     })
